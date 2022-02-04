c871e439de23c0356d0db0dee03adcc400bfddae
evn <evn@google.com>
2022-02-04 15:34:00 +0100

    quote commit

--- src/index.ts
+++ src/index.ts
@@ -21,10 +21,10 @@ async function load() {
   const commit = location.hash.slice(1) || '618f003199c6188e01472b03cdbba227f1dc5f24';
 
   const result = {
-    tags: await worker.db.query("SELECT tags FROM tags WHERE commit = ?", [commit]),
-    reported_by: await worker.db.query("SELECT reported_by FROM reported_by WHERE commit = ?", [commit]),
-    fixes: await worker.db.query("SELECT fixes FROM fixes WHERE commit = ?", [commit]),
-    backports: await worker.db.query("SELECT commit FROM upstream WHERE upstream = ?", [commit]),
+    tags: await worker.db.query("SELECT tags FROM tags WHERE `commit` = ?", [commit]),
+    reported_by: await worker.db.query("SELECT reported_by FROM reported_by WHERE `commit` = ?", [commit]),
+    fixes: await worker.db.query("SELECT fixes FROM fixes WHERE `commit` = ?", [commit]),
+    backports: await worker.db.query("SELECT `commit` FROM upstream WHERE upstream = ?", [commit]),
   };
 
   document.body.textContent = JSON.stringify(result);
