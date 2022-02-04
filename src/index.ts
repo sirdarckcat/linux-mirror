b4e9b321d5980622ca7fe055ab03f63d1827d9ed
evn <evn@google.com>
2022-02-04 13:06:49 +0100

    add fresh repo

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import {createDbWorker} from "sql.js-httpvfs";
+
+const workerUrl = new URL(
+  "sql.js-httpvfs/dist/sqlite.worker.js",
+  import.meta.url
+);
+const wasmUrl = new URL("sql.js-httpvfs/dist/sql-wasm.wasm", import.meta.url);
+
+async function load() {
+  const worker = await createDbWorker(
+    [
+      {
+        from: "jsonconfig",
+        configUrl: "../config.json"
+      },
+    ],
+    workerUrl.toString(),
+    wasmUrl.toString()
+  );
+
+  const result = await worker.db.query(`select * from reported_by`);
+
+  document.body.textContent = JSON.stringify(result);
+}
+
+load();
