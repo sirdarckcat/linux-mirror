0ee034648ad366c1230527874671d31941fea4b4
evn <evn@google.com>
2022-02-04 16:34:53 +0100

    show more info

--- src/index.ts
+++ src/index.ts
@@ -18,16 +18,22 @@ async function load() {
     wasmUrl.toString()
   );
 
-  const commit = location.hash.slice(1) || '618f003199c6188e01472b03cdbba227f1dc5f24';
+  const commit = location.hash.slice(1) || 'e1b3fa7b6471e1b2f4c7573711e7f8ee2e9f3dc3';
 
   const result = {
+    commit,
     tags: await worker.db.query("SELECT tags FROM tags WHERE `commit` = ?", [commit]),
     reported_by: await worker.db.query("SELECT reported_by FROM reported_by WHERE `commit` = ?", [commit]),
     fixes: await worker.db.query("SELECT fixes FROM fixes WHERE `commit` = ?", [commit]),
     backports: await worker.db.query("SELECT `commit` FROM upstream WHERE upstream = ?", [commit]),
+    upstream: await worker.db.query("SELECT upstream FROM upstream WHERE `commit` = ?", [commit]),
+    backportsTags: await worker.db.query("SELECT tags FROM tags WHERE `commit` IN (SELECT `commit` FROM upstream WHERE upstream = ?)", [commit]),
+    fixesTags: await worker.db.query("SELECT tags FROM tags JOIN (SELECT substr(fixes, 0, instr(fixes, ' ')) trunc FROM fixes WHERE `commit` = ?) ON (substr(`commit`, 1, length(trunc))=trunc)", [commit]),
+    fixesBackportsTags: await worker.db.query("SELECT tags FROM tags WHERE `commit` IN (SELECT `commit` FROM upstream JOIN (SELECT substr(fixes, 0, instr(fixes, ' ')) trunc FROM fixes WHERE `commit` = ?) ON (substr(upstream, 1, length(trunc))=trunc))", [commit]),
   };
 
-  document.body.textContent = JSON.stringify(result);
+  document.body.style.whiteSpace = 'pre-wrap';
+  document.body.textContent = JSON.stringify({...result}, null, 1);
 }
 
 load();
