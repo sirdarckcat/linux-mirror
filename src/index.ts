8924432b635c7ab3ff5bbec750fc4c06659a86f9
Eduardo' Vela" Nava (sirdarckcat) <evn@google.com>
2022-02-05 22:58:03 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -18,20 +18,35 @@ async function load() {
     wasmUrl.toString()
   );
 
-  const commit = location.hash.slice(1) || 'e1b3fa7b6471e1b2f4c7573711e7f8ee2e9f3dc3';
+  const commit = location.hash.slice(1);
+
+  if(!commit) {
+    location.hash = "e1b3fa7b6471e1b2f4c7573711e7f8ee2e9f3dc3";
+    location.reload();
+    return;
+  }
+
+  const results = await Promise.all([
+    worker.db.query("SELECT tags FROM tags WHERE `commit` = ?", [commit]),
+    worker.db.query("SELECT tags FROM tags WHERE `commit` IN (SELECT `commit` FROM upstream WHERE upstream = ?)", [commit]),
+    worker.db.query("SELECT fixes FROM fixes WHERE `commit` = ?", [commit]),
+    worker.db.query("SELECT tags FROM tags JOIN (SELECT substr(fixes, 0, instr(fixes, ' ')) trunc FROM fixes WHERE `commit` = ? AND LENGTH(fixes)>4) ON (`commit`>trunc AND `commit`<trunc||'g')", [commit]),
+    worker.db.query("SELECT tags FROM tags WHERE `commit` IN (SELECT `commit` FROM upstream JOIN (SELECT substr(fixes, 0, instr(fixes, ' ')) trunc FROM fixes WHERE `commit` = ? AND LENGTH(fixes)>4) ON (upstream>trunc AND upstream<trunc||'g'))", [commit]),
+    worker.db.query("SELECT reported_by FROM reported_by WHERE `commit` = ?", [commit])
+  ]);
 
   const result = {
     commit,
-    "commit landed on upstream on": await worker.db.query("SELECT tags FROM tags WHERE `commit` = ?", [commit]),
-    "commit was backported to": await worker.db.query("SELECT tags FROM tags WHERE `commit` IN (SELECT `commit` FROM upstream WHERE upstream = ?)", [commit]),
-    "commit fixes bug introduced by": await worker.db.query("SELECT fixes FROM fixes WHERE `commit` = ?", [commit]),
-    "buggy commit landed on upstream on": await worker.db.query("SELECT tags FROM tags JOIN (SELECT substr(fixes, 0, instr(fixes, ' ')) trunc FROM fixes WHERE `commit` = ? AND LENGTH(fixes)>4) ON (`commit`>trunc AND `commit`<trunc||'g')", [commit]),
-    "buggy commit was backported to": await worker.db.query("SELECT tags FROM tags WHERE `commit` IN (SELECT `commit` FROM upstream JOIN (SELECT substr(fixes, 0, instr(fixes, ' ')) trunc FROM fixes WHERE `commit` = ? AND LENGTH(fixes)>4) ON (upstream>trunc AND upstream<trunc||'g'))", [commit]),
-    "syzkaller reporter email": await worker.db.query("SELECT reported_by FROM reported_by WHERE `commit` = ?", [commit])
+    "commit landed on upstream on": results[0],
+    "commit was backported to": results[1],
+    "commit fixes bug introduced by": results[2],
+    "buggy commit landed on upstream on": results[3],
+    "buggy commit was backported to": results[4],
+    "syzkaller reporter email": results[5]
   };
 
   document.body.style.whiteSpace = 'pre-wrap';
-  document.body.textContent = JSON.stringify({...result}, null, 1);
+  document.body.textContent = JSON.stringify(result, null, 1);
 }
 
 load();
