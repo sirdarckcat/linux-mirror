77b62494197cc69648ad0af6899c05529fa103c1
Eduardo' Vela" Nava (sirdarckcat) <evn@google.com>
2022-02-06 11:30:34 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ async function load() {
     wasmUrl.toString()
   );
 
-  const commit = location.hash.slice(1);
+  let commit = location.hash.slice(1);
 
   if(!commit) {
     location.hash = "e1b3fa7b6471e1b2f4c7573711e7f8ee2e9f3dc3";
@@ -26,29 +26,48 @@ async function load() {
     return;
   }
 
+  let githubCommit = null;
+  const getGithubCommit = ()=>await (await fetch(`https://api.github.com/repos/sirdarckcat/linux-1/commits/${commit}`)).json();
+
+  if(commit.length < 40) {
+    githubCommit = await getGithubCommit();
+    if (!githubCommit.sha) throw new Error("Couldn't find commit " + commit);
+    commit = githubCommit.sha;
+  }
+
   const results = await Promise.all([
+    githubCommit || getGithubCommit ()
     worker.db.query("SELECT tags FROM tags WHERE `commit` = ?", [commit]),
+    worker.db.query("SELECT upstream FROM upstream WHERE `commit` = ?", [commit]),
     worker.db.query("SELECT tags, `commit` FROM tags WHERE `commit` IN (SELECT `commit` FROM upstream WHERE upstream = ?)", [commit]),
-    worker.db.query("SELECT fixes FROM fixes WHERE `commit` = ?", [commit]),
-    worker.db.query("SELECT tags, `commit` FROM tags JOIN (SELECT substr(fixes, 0, instr(fixes, ' ')) trunc FROM fixes WHERE `commit` = ? AND LENGTH(fixes)>4) ON (`commit`>trunc AND `commit`<trunc||'g')", [commit]),
-    worker.db.query("SELECT tags, `commit` FROM tags WHERE `commit` IN (SELECT `commit` FROM upstream JOIN (SELECT substr(fixes, 0, instr(fixes, ' ')) trunc FROM fixes WHERE `commit` = ? AND LENGTH(fixes)>4) ON (upstream>trunc AND upstream<trunc||'g'))", [commit]),
-    worker.db.query("SELECT tags, `commit` FROM tags WHERE `commit` IN (SELECT `commit` FROM fixes WHERE LENGTH(fixes)>4 AND fixes >= substr(?, 1, 4) AND fixes <= ?)", [commit, commit]),
-    worker.db.query("SELECT reported_by, `commit` FROM reported_by WHERE `commit` = ? OR `commit` IN (SELECT `commit` FROM fixes WHERE LENGTH(fixes)>4 AND fixes >= substr(?, 1, 4) AND fixes <= ?)", [commit, commit, commit]),
+    worker.db.query("SELECT fixes FROM fixes WHERE `commit` = ? AND LENGTH(fixes)>=4", [commit]),
+    worker.db.query("SELECT tags, `commit` FROM tags JOIN (SELECT substr(fixes, 0, instr(fixes, ' ')) trunc FROM fixes WHERE `commit` = ? AND LENGTH(fixes)>=4) ON (`commit`>trunc AND `commit`<trunc||'g')", [commit]),
+    worker.db.query("SELECT tags, `commit` FROM tags WHERE `commit` IN (SELECT `commit` FROM upstream JOIN (SELECT substr(fixes, 0, instr(fixes, ' ')) trunc FROM fixes WHERE `commit` = ? AND LENGTH(fixes)>=4) ON (upstream>trunc AND upstream<trunc||'g'))", [commit]),
+    worker.db.query("SELECT tags, `commit` FROM tags WHERE `commit` IN (SELECT `commit` FROM fixes WHERE LENGTH(fixes)>=4 AND fixes >= substr(?, 1, 4) AND fixes <= ?)", [commit, commit]),
+    worker.db.query("SELECT reported_by, `commit` FROM reported_by WHERE `commit` = ? OR `commit` IN (SELECT `commit` FROM fixes WHERE LENGTH(fixes)>=4 AND fixes >= substr(?, 1, 4) AND fixes <= ?)", [commit, commit, commit]),
   ]);
 
   const result = {
     commit,
-    "the commit landed on upstream on": results[0],
-    "the commit was backported to": results[1],
-    "the commit fixes a bug introduced by": results[2],
-    "the buggy commit landed on upstream on": results[3],
-    "the buggy commit was backported to": results[4],
-    "the commit introduced a bug fixed by": results[5],
-    "syzkaller reference for the commit and the fix commit": results[6]
+    "details": results.unshift().commit.message.split("\n"),
+    "the commit landed on upstream on": results.unshift(),
+    "the commit is a backport of": results.unshift(),
+    "the commit was backported to": results.unshift(),
+    "the commit fixes a bug introduced by": results.unshift(),
+    "the buggy commit landed on upstream on": results.unshift(),
+    "the buggy commit was backported to": results.unshift(),
+    "the commit introduced a bug fixed by": results.unshift(),
+    "syzkaller reference for the commit and the fix commit": results.unshift()
   };
 
   document.body.style.whiteSpace = 'pre-wrap';
   document.body.textContent = JSON.stringify(result, null, 1);
 }
 
-load();
+(async function () {
+  try {
+    load();
+  } catch(e) {
+    alert(e);
+  }
+});
